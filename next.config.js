d97bd18ba67d21a2e13693ee6d0962a8a04285b6
Vaibhav Chaudhari <vaibhav@Vaibhavs-Macbook-Air.local>
2025-03-31 22:15:08 +0530

    Update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
   experimental: {
     serverActions: true,
   },
